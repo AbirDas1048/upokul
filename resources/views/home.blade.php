4af0e1448632a0cec2ccce3c4bc989dfc5c27af6
Abir Das <abirdas422@gmail.com>
2026-02-02 01:06:25 +0600

    title and fav icon added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Travel Agency Landing Page</title>
+    <title>{{ config('app.name') }}</title>
+    <link rel="icon" type="image/x-icon" href=" {{ asset('home/images/favicon.ico') }}">
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Google Fonts -->
