901b8237370255753f3a977a823ca53ef7926c55
Abir Das <abirdas422@gmail.com>
2026-05-12 20:27:51 +0600

    unused file removed

--- resources/views/demo1.blade.php
+++ resources/views/demo1.blade.php
@@ -97,6 +97,6 @@
 <!-- Scripts -->
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="{{ asset('js/common-interactions.js') }}"></script>
+<script src="{{ asset('home/js/common-interactions.js') }}"></script>
 </body>
 </html>
