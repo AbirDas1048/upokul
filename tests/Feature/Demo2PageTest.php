a98cd8a15087c2a1c353007bfc8e81340be88e7d
Abir Das <abirdas422@gmail.com>
2026-06-12 13:27:50 +0600

    demo2 feature test added

---
+++ tests/Feature/Demo2PageTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class Demo2PageTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_demo2_page_loads_successfully(): void
+    {
+        $response = $this->get('/demo2');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_demo2_named_route_loads_successfully(): void
+    {
+        $response = $this->get(route('demo2'));
+
+        $response->assertOk();
+    }
+}
