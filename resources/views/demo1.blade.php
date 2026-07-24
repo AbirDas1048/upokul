e1a47ab5413b9adc8640c42266359a34fbb485d8
Abir Das <abirdas422@gmail.com>
2026-07-24 22:47:48 +0600

    demo1 hero responsive done

--- resources/views/demo1.blade.php
+++ resources/views/demo1.blade.php
@@ -28,7 +28,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-lg-6 hero-content">
+            <div class="col-12 col-lg-10 col-xl-8 mx-auto hero-content">
                 <h1>
                     Discover Beautiful Places <br>
                     <span>With {{ config('app.name') }}</span>
